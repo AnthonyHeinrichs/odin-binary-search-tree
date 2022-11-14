79806aee0f4b36c60c683775a1a5b9db590787f0
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-14 15:03:13 +0100

    Now sorting the passed array and removing duplicates before building tree

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 import buildTree from "./build-tree.js";
+import { mergeSort } from "./merge-sort.js"
 
 const Node = (data, leftChild = null, rightChild = null) => {
   return { data, leftChild, rightChild };
 };
 
 const Tree = (arr) => {
-  const root = buildTree(arr);
+  // Making sure array is sorted by using merge sort algorithm 
+  let sortedArray = mergeSort(arr)
+  // Removing duplicates values from array
+  sortedArray = [...new Set(sortedArray)]
+  // Getting root node and building binary tree
+  const root = buildTree(sortedArray);
+
+  
 
   return {
     get root() {
@@ -14,7 +22,7 @@ const Tree = (arr) => {
   };
 };
 
-const newTree = Tree([1, 2, 3, 4, 5]);
+const newTree = Tree([5, 4, 3, 3, 2, 1, 9, 7]);
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node.rightChild !== null) {
